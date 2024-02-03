c6b1cd3ec9f100fbf232f632f68df5359dc392ff
CoolKavin123 <154696192+CoolKavin123@users.noreply.github.com>
2024-02-03 13:02:28 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+function mercury(){
+    window.open("https://en.wikipedia.org/wiki/Mercury_(planet)")
+}
+
+function venus(){
+    window.open("https://en.wikipedia.org/wiki/Venus")
+}
+
+function earth(){
+    window.open("https://en.wikipedia.org/wiki/Earth")
+}
+
+function mars(){
+    window.open("https://en.wikipedia.org/wiki/Mars")
+}
+
+function saturn(){
+    window.open("https://en.wikipedia.org/wiki/Saturn")
+}
+
+function jupiter(){
+    window.open("https://en.wikipedia.org/wiki/Jupiter")
+}
+
+function neptune(){
+    window.open("https://en.wikipedia.org/wiki/Neptune")
+}
+
+function uranus(){
+    window.open("https://en.wikipedia.org/wiki/Uranus")
+}
+
+function pluto(){
+    window.open("https://en.wikipedia.org/wiki/Pluto")
+}
